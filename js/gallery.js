7f15cc1366658cd8e609a539c5f53179464a858c
nicholasaharwell-HWS <nicholas.a.harwell@gmail.com>
2026-07-05 16:53:19 -0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,12 @@
+document.querySelectorAll(".ba-slider").forEach((slider) => {
+  const range = slider.querySelector(".ba-range");
+  const beforeWrap = slider.querySelector(".ba-before-wrap");
+  const handle = slider.querySelector(".ba-handle");
 
+  if (!range || !beforeWrap || !handle) return;
+
+  range.addEventListener("input", () => {
+    beforeWrap.style.width = `${range.value}%`;
+    handle.style.left = `${range.value}%`;
+  });
+});
